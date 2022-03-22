a03d622832d13fc587002adb4ea091d718580705
Geonil Goh <hanulgoh@gmail.com>
2022-03-22 17:09:36 +0900

    Implement getDisplayState()

--- lib/Visual.d.ts
+++ lib/Visual.d.ts
@@ -1,5 +1,6 @@
 export declare type DisplayState = "on" | "off";
 declare abstract class Visual {
+    static getDisplayState(): DisplayState;
     static setDisplayState(state: DisplayState, display?: string): void;
 }
 export default Visual;
--- src/Visual.ts
+++ src/Visual.ts
@@ -1,10 +1,17 @@
-import { exec } from "child_process";
+import { exec, execSync } from "child_process";
 
 export type DisplayState = "on" | "off";
 
 abstract class Visual {
+  static getDisplayState(): DisplayState {
+    const result = execSync("xset q").toString();
+    if (result.includes("Monitor is On")) return "on";
+    if (result.includes("Monitor is Off")) return "off";
+    throw new Error("unable to get display state");
+  }
+
   static setDisplayState(state: DisplayState, display = ":0") {
-    exec(`DISPLAY=${display} xset dpms force ${state}`);
+    execSync(`DISPLAY=${display} xset dpms force ${state}`);
   }
 }
 
